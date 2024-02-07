ef4b95958f71b12c1e456cbb8d129c2d582def5f
Advay <advaykumar2004@gmail.com>
2024-02-07 14:57:57 +1100

    first commit

---
+++ Assets/Script/SelectionHighlight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionHighlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void advay()
+    {
+        Debug.Log("Advay Selected the cube he put in");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
